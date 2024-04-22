c4ec94bbdb4e04f394e228631d6079322bd9f39b
Dream Of Future <hlainghtun2018@gmail.com>
2024-04-22 17:38:21 +0630

    update

--- src/com/sspd/Method/Ex_1.java
+++ src/com/sspd/Method/Ex_1.java
@@ -3,5 +3,20 @@ package com.sspd.Method;
 public class Ex_1 {
     public static void main(String[] args) {
 
+        getInfo();// static to static
+
+        new Ex_1().getPrint();// static to non static
+    }
+
+    static void getInfo(){
+
+        System.out.println("We are Developer");
+
+    }
+
+    void getPrint(){
+
+        System.out.println("I am Thu Thu");
+
     }
 }
